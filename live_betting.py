c99ffe08d9bffaaf251e2a4f99dc2d8b5730ebb6
Mikkel Jensen <mjensen@Mikkels-MacBook-Pro.local>
2018-03-06 12:47:26 +0100

    added first comparison of stats to odds

--- live_betting.py
+++ live_betting.py
@@ -14,21 +14,26 @@ driver = webdriver.PhantomJS()
 driver.get(page_url)
 
 # This will get the initial html - before javascript
-html1 = driver.page_source
+driver.page_source
 time.sleep(5) # sleep to make sure the page is loaded before executing java
 
 # This will get the html after on-load javascript
-html2 = driver.execute_script("return document.documentElement.innerHTML;")
+html = driver.execute_script("return document.documentElement.innerHTML;")
 
-code_string = unicodedata.normalize('NFKD', html2).encode('ascii','ignore')
+code_string = unicodedata.normalize('NFKD', html).encode('ascii','ignore')
 live_urls = []
 
 for i_c in code_string.split('js-event-link js-event'):
 	live_urls.append("https://www.sofascore.com" + str(i_c[0:300].split('href="')[-1].split('" data')[0][3:]))
 
-
+no_stats_matches = 0 # counter for matches without statistics
 
 for i_match in live_urls[1:]:
+
+	if no_stats_matches > 2:
+		print '3 matches in a row without statistics. STOPPING SCRIPT'
+		break
+
 	print 'getting match with link : %s' %i_match
 
 	try:
@@ -50,18 +55,22 @@ for i_match in live_urls[1:]:
 		
 		data = [i.split('[')[0] for i in json_file['statistics'].keys()]
 		for i in json_file['odds']:
-			print '---------------------'
-			
-			for j in i['live']:
-				for k in j['odds']:
-					print k['decimalValue']
-					odds.append(k['decimalValue'])
+			if i['name'] == 'Next goal':
+				print '---------------------'
+				for j in i['live']:
+					for k in j['odds']:
+						print k['decimalValue']
+						odds.append(k['decimalValue'])
 
 		
 	except:
+
+		no_stats_matches += 1
 		print 'could not get data from this match'
 		continue
 	
+	no_stats_matches = 0
+
 	data_frame = []
 	for i_k in range(0,len(data)):
 		
@@ -70,19 +79,61 @@ for i_match in live_urls[1:]:
 			data_frame.append([data[i_k],json_file['statistics'][data[i_k]]])
 		else: continue
 
+	# rearange the data_frame a bit
 	data_frame = pd.DataFrame(data_frame)
 	data_frame = data_frame[data_frame[0] != 'provider']
 	data_frame = data_frame.sort_values(by=[0])
-	print data_frame # 0 is names on variables, 1 is variable value
+	data_frame.columns = ['variable','value']
+	data_frame = data_frame.set_index('variable')
+	#print data_frame
 
-	ticks = [unicodedata.normalize('NFKD', i).encode('ascii','ignore') for i in data_frame[0]]
-	plt.plot(data_frame[1][0:len(data_frame)/2],'o',label='away')
-	plt.plot(data_frame[1][len(data_frame)/2:],'+',label='home')
+	significance = 1.5 # how well does a team need to perform before the script should tell us?
+
+	TotalShotsOnGoal = float(data_frame["value"]["homeTotalShotsOnGoal"] + data_frame["value"]["awayTotalShotsOnGoal"])
+	print TotalShotsOnGoal, data_frame["value"]["homeTotalShotsOnGoal"], data_frame["value"]["awayTotalShotsOnGoal"]
+	
+	if float(odds[0]) * data_frame["value"]["homeTotalShotsOnGoal"] / TotalShotsOnGoal > significance :
+		print "interesting odds on home team next goal"
+
+	if float(odds[2]) * data_frame["value"]["awayTotalShotsOnGoal"] / TotalShotsOnGoal > significance : 	
+		print "interesting odds on away team next goal" 
+
+	"""
+	# plotting
+	ticks = [unicodedata.normalize('NFKD', i).encode('ascii','ignore') for i in data_frame['value']]
+	plt.plot(data_frame['value'][0:len(data_frame)/2],'o',label='away')
+	plt.plot(data_frame['value'][len(data_frame)/2:],'+',label='home')
 	plt.legend()
 	plt.xticks(np.linspace(0,len(ticks)/2,len(ticks)/2),ticks[0:len(ticks)/2],rotation=90)
 	plt.show(block=False)
+	"""
 
 raw_input( ' ... ' )
+
+####
+# TODO:
+# Compare with next goal odds.
+# Use Ball possession, red cards, shots on goal, shots of goal as stats
+# compare to previous half.
+# get asain odds from pages.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
