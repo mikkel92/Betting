f95bdc73755db2b5ceeaba8b01bd942ba4995cd8
Mikkel Jensen <mjensen@Mikkels-MacBook-Pro.local>
2018-08-06 13:04:06 +0200

    updated script

--- live_betting/live_bet365.py
+++ live_betting/live_bet365.py
@@ -9,11 +9,40 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchAttributeException
 from selenium.webdriver.common.keys import Keys
 import urllib, json
-import pyautogui
 from datetime import datetime
 
+def get_match_data(button,browser):
 
-def get_live_urls():
+	# click mathc button and wait a bit for it to load
+	start_time = datetime.now()
+	button.click()
+	time.sleep(0.2)
+	inner_html = str(button.get_attribute('innerHTML').encode(encoding='UTF-8',errors='strict'))
+
+	# change to stats tab i live stream is an option for the match
+	if "ipn-ScoreDisplayStandard_AVIcon" in inner_html:
+		stats_tab = browser.find_element_by_class_name("lv-ButtonBar_MatchLive")
+		stats_tab.click()
+		time.sleep(0.2)
+
+	# try to scrape the match data
+	try:
+		event_data = browser.find_elements_by_class_name("ml1-AllStats")
+		event_odds = browser.find_elements_by_class_name("ipe-EventViewDetail_MarketGrid")
+		match_data = ([event_data[0].text, event_odds[0].text])
+
+	except: 
+		match_data = "failed"
+
+	if "ipn-ScoreDisplayStandard " not in inner_html:
+		match_data = "not_soccer_match"
+	
+	time.sleep(np.random.rand()*0.5)
+	return match_data
+
+
+
+def scrape_betting():
 
 	page_url_mobile = "https://mobile.bet365.dk/#type=InPlay;"
 	page_url = "https://www.bet365.dk/#/IP/"
@@ -22,36 +51,49 @@ def get_live_urls():
 	browser.get(page_url) # get the url for the corrosponding league
 	browser.get(page_url)
 
-	# Get browser to front of your screen
-	pyautogui.moveTo(100, 200) # make the window come to front of screen for mouseover
-	pyautogui.dragTo(100, 200, button='left')
-	time.sleep(2)
+	time.sleep(3) #
 
 	# Click on the tab to web scrape
 	se_begivenhed_button = browser.find_elements_by_class_name("ip-ControlBar_BBarItem")
 	se_begivenhed_button[1].click()
-	time.sleep(2)
+	time.sleep(3)
 
 	
 	
 	# Click on every live event in the live betting tab
 	event_buttons = browser.find_elements_by_class_name("ipn-FixtureButton")
-	page_fails = ([len(event_buttons),0])
-	for button in event_buttons:
+	failed_loads = []
+	page_fails = ([0,0])
+
+	for counter, button in enumerate(event_buttons):
+		print counter
+
+		match_data = get_match_data(button,browser)
 		
-		try:
-			start_time = datetime.now()
-			button.click()
-			event_data = browser.find_elements_by_class_name("ipe-EventViewDetail_MarketGrid")
-			match_data = event_data[0].text
-			time.sleep(np.random.rand()*0.5 +.2)
-		
-		except:
-			page_fails[1] += 1
+		if match_data == "not_soccer_match":
+			print "Done scraping soccer matches"
+			break
+		elif match_data == "failed":
+			match_data = get_match_data(button,browser)
+			if match_data == "failed":
+				failed_loads.append(button)
+				print "failed to get page"
+				continue
+		else:
 			continue
+	print match_data	
+		
 	
+	for button in failed_loads:
+
+		match_data = get_match_data(button,browser)
+		if match_data == "failed":
+			page_fails[1] += 1
+			print "failed to get page second time"
+			
+
 	print page_fails
-	source = browser.page_source
+
 	#print source
 	browser.close()
 	"""
@@ -63,6 +105,6 @@ def get_live_urls():
 	#print html
 
 	"""
-get_live_urls()
+scrape_betting()
 
 
